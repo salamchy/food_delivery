42f16841513cb127ce489568f4b8ed5a89b6d856
salam <chaudharysm00@gmail.com>
2024-11-01 22:47:50 +0545

    interface menu model

--- src/models/menu.model.ts
+++ src/models/menu.model.ts
@@ -1,5 +1,18 @@
 import mongoose from "mongoose";
 
+export interface IMenu {
+  _id: mongoose.Schema.Types.ObjectId;
+  name: String;
+  description: String;
+  price: Number;
+  image: String;
+}
+
+export interface IMenuDocument extends IMenu, Document {
+  createdAt: Date;
+  updatedAt: Date;
+}
+
 const menuSchema = new mongoose.Schema({
   
 })
