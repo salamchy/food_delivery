4ea9fff1fd234890e51ca8e56e3a52a59aa3a183
salam <chaudharysm00@gmail.com>
2024-11-02 23:43:46 +0545

    user controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,8 +1,16 @@
 import { Request, Response } from "express";
+import { User } from "../models/user.model";
 
 export const signup = async(req:Request, res:Response) => {
   try {
-    const { fullname, email, password, contact } = 
+    const { fullname, email, password, contact } = req.body;
+
+    let user = await User.findOne({email});
+    if (user) {
+      return res.status(400).json({success: false,
+        message:"User already exist"
+      })
+    }
   } catch (error) {
     console.log(error);
     return res.status(500).json({message: "Internal Server Error"})
