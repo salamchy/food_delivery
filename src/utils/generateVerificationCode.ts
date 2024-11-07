7a33880a8f0f55dbc0173c8a88c64126ae6ba584
salam <chaudharysm00@gmail.com>
2024-11-07 18:29:57 +0545

    generate verification code of 6 digits

---
+++ src/utils/generateVerificationCode.ts
@@ -0,0 +1,11 @@
+export const generateVerificationCode = (length: 6): String => {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let verificationCode = '';
+  const charactersLength = characters.length;
+
+  for (let i = 0; i < length; i++) {
+    verificationCode += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return verificationCode;
+}
