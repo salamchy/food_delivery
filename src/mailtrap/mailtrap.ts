457b7e8baf3501b9ad4ca03ab9c0f918f6cf1406
salam <chaudharysm00@gmail.com>
2024-11-09 21:00:18 +0545

    mailtrap

---
+++ src/mailtrap/mailtrap.ts
@@ -0,0 +1,10 @@
+import { MailtrapClient } from "mailtrap";
+
+export const client = new MailtrapClient({
+  token: process.env.MAILTRAP_API_TOKEN!,
+});
+
+export const sender = {
+  email: "hello@demomailtrap.com",
+  name: "CDYEATS",
+};
