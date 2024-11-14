f815e5a2d811e17aa79af5bf1131395eb60ba9e9
salam <chaudharysm00@gmail.com>
2024-11-14 16:51:06 +0545

    resturant api

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,37 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import userRouter from "./routes/user.route";
+import resturantRouter from "./routes/resturant.route";
+import bodyParser from "body-parser"
 
 // Create an instance of an Express application
-const application = express();
+const app = express();
 
 // Enable CORS with specific options
 // - origin: allows requests from the specified origin, which is set via environment variable
 // - credentials: allows cookies to be included in cross-origin requests
-application.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+  credentials: true
 }));
 
+//body parser
+app.use(bodyParser.json({ limit: '10mb' }));
+
 // Parse incoming JSON requests with a body size limit of 16kb
-application.use(express.json({ limit: "16kb" }));
+app.use(express.json({ limit: "10mb" }));
 
 // Parse incoming URL-encoded data with a body size limit of 16kb
 // - extended: true allows for rich objects and arrays to be encoded into the URL-encoded format
-application.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 // Serve static files from the "public" directory
-application.use(express.static("public"));
+app.use(express.static("public"));
 
 // Parse cookies from incoming requests and populate req.cookies
-application.use(cookieParser());
+app.use(cookieParser());
+
+//api
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/resturant", resturantRouter);
