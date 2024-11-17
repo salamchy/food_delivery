4b1f88490efeda45b299651094b42b193c6d1f3e
salam <chaudharysm00@gmail.com>
2024-11-17 22:01:01 +0545

    stripe

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from "express";
 import { Resturant } from "../models/resturant.model";
 import { Order } from "../models/order.model";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
 type checkOutSessionRequest = {
   cartItems: {
@@ -20,6 +23,7 @@ type checkOutSessionRequest = {
 
   resturantId: string
 }
+
 
 export const createCheckOutSession = async (req: Request, res: Response): Promise<void> => {
   try {
@@ -42,6 +46,37 @@ export const createCheckOutSession = async (req: Request, res: Response): Promis
       status: "pending"
     });
 
+    const menuItems = resturant.menus;
+    const lineItems = createLineItems(checkOutSessionRequest, menuItems);
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      shipping_address_collection: {
+        allowed_countries: ['GB', 'US', 'CA']
+      },
+      line_items: lineItems,
+      mode: 'payment',
+      success_url: `${process.env.FRONTEND_URL}/order/status`,
+      cancel_url: `${process.env.FRONTEND_URL}/cart`,
+      metadata: {
+        orderId: order._id.toString(),
+        images: JSON.stringify(menuItems.map((item: any) => item.image))
+      }
+    });
+    if (!session.url) {
+      res.status(400).json({
+        success: false,
+        message: "Error while creating session."
+      })
+      return;
+    }
+    await order.save();
+    res.status(200).json({
+      session
+
+    })
+    return;
+
   } catch (error) {
     // Log the error for debugging purposes
     console.error(error);
@@ -49,4 +84,25 @@ export const createCheckOutSession = async (req: Request, res: Response): Promis
     // Send a server error response
     res.status(500).json({ message: "Internal Server Error" });
   }
+}
+
+export const createLineItems = (checkOutSessionRequest: checkOutSessionRequest, menuItems: any) => {
+  const lineItems = checkOutSessionRequest.cartItems.map((cartItem) => {
+    const menuItem = menuItems.find((item: any) =>
+      item._id === cartItem.menuId
+    );
+    if (!menuItem) throw new Error(`Menu item id not found`);
+    return {
+      price_data: {
+        currency: 'usd',
+        product_data: {
+          name: menuItem.name,
+          images: [menuItem.image]
+        },
+        unit_amount: menuItem.price
+      },
+      quantity: cartItem.quantity,
+    }
+  });
+  return lineItems;
 }
